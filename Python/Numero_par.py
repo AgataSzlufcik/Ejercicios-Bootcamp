35f96dde2b649d21507e1cf4209f542d5eaa500e
AGATA <agata.szlufcik@gmail.com>
2026-03-04 21:59:15 +0100

    Ejercicios de bootcamp

---
+++ Python/Numero_par.py
@@ -0,0 +1,10 @@
+
+numero_usuario = int(input("Ingresa un numero de 1-100: "))
+if numero_usuario %2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
+
+
+
+
---
+++ Python/adivinar_numero.py
@@ -0,0 +1,35 @@
+#importar libreria 
+import random
+
+#1. Generar un número en el rango numerico del 0 al 100 numer=(1,100)
+numero_secreto = random.randint(1,100)
+
+#2. Crear un contador de intentos con valor inicial=0.
+contador = 0
+
+#3. Crear un mensaje de bienvenida al usuario
+print("Bienvenido en el juego! Adivina el numero secreto del 1 al 100: ") 
+
+#4. Crear el bucle
+while True:
+    
+    #5. Pedir al usuario que ingrese un numero
+    numero_usuario = int(input("Ingresa un numero de 1-100: "))
+    #6. Incrementar el contador de intentos
+    contador += 1
+    #7. Comparar el numero ingresado con el numero secreto
+    if numero_usuario > numero_secreto:
+        print("El numero secreto es menor.")
+    elif numero_usuario < numero_secreto:
+        print("El numero secreto es mayor.")
+    else:
+        print(f"¡Felicidades! Adivinaste el numero en {contador} intentos.")
+        seguir_jugando = input("¿Quieres jugar de nuevo? (si/no): ")
+        if seguir_jugando == "si":
+            continue
+        else:
+            print("Gracias por jugar! Nos vemos en el otro juego")
+            break
+
+    
+  
