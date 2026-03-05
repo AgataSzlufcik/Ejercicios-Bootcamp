606ce96c0a5060e1e389e5e1683621cc902fad72
AGATA <agata.szlufcik@gmail.com>
2026-03-05 12:24:24 +0100

    Ejercicio numero par mejorado

--- Python/Numero_par.py
+++ Python/Numero_par.py
@@ -1,9 +1,23 @@
 
-numero_usuario = int(input("Ingresa un numero de 1-100: "))
-if numero_usuario %2 == 0:
-    print("El numero es par")
+numero_usuario = None
+
+while numero_usuario is None or numero_usuario < 1 or numero_usuario > 100:
+    try:
+        numero_usuario = int(input("Ingresa un número de 1-100: "))
+        
+        if numero_usuario < 1 or numero_usuario > 100:
+            print("Número fuera de rango")
+    except ValueError:
+        print("Debes escribir un numero")
+
+
+if numero_usuario % 2 == 0:
+    print("El número es par")
 else:
-    print("El numero es impar")
+    print("El número es impar")
+
+
+
 
 
 
